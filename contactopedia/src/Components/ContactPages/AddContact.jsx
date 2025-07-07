2eed4b007c64e5634dcf6608c010c5201057ec1d
bhrugen <bhrugen1990@gmail.com>
2025-07-07 08:38:25 -0500

    Section 5 - Handle Form Submit

--- contactopedia/src/Components/ContactPages/AddContact.jsx
+++ contactopedia/src/Components/ContactPages/AddContact.jsx
@@ -1,7 +1,12 @@
 function AddContact() {
+  function handleAddContactForm(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
     <div className="border col-12 text-white p-2">
-      <form>
+      <form onSubmit={handleAddContactForm}>
         <div className="row p-2">
           <div className="col-12 text-white-50 text-center h5">
             Add a new Contact
@@ -9,17 +14,20 @@ function AddContact() {
           <div className="col-12 col-md-4 p-1">
             <input
               placeholder="Name..."
+              name="name"
               className="form-control form-control-sm"
             />
           </div>
           <div className="col-12 col-md-4 p-1">
             <input
+              name="email"
               placeholder="Email..."
               className="form-control form-control-sm"
             />
           </div>
           <div className="col-12 col-md-4 p-1">
             <input
+              name="phone"
               placeholder="Phone..."
               className="form-control form-control-sm"
             />
@@ -27,7 +35,10 @@ function AddContact() {
           <div className="col-12 text-center text-success">Success Message</div>
           <div className="col-12 text-center text-danger">Error Message</div>
           <div className="col-12">
-            <button className="btn btn-primary btn-sm form-control">
+            <button
+              type="submit"
+              className="btn btn-primary btn-sm form-control"
+            >
               Create
             </button>
           </div>
