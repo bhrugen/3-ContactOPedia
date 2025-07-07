0970bba04d927a6b248d00b33569c6ae76052cca
bhrugen <bhrugen1990@gmail.com>
2025-07-07 11:39:21 -0500

    Section 6 - Utility to call API

---
+++ contactopedia/src/Utility/api.jsx
@@ -0,0 +1,15 @@
+const getRandomUser = async () => {
+  const url = new URL("https://randomuser.me/api/");
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
+export default getRandomUser;
